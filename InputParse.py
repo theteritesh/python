5649691e0b0293515da101b894fd7a35a69d8e03
theteritesh <theteritesh2@gmail.com>
2023-12-23 15:18:57 +0530

    Taking Input From User

---
+++ InputParse.py
@@ -0,0 +1,19 @@
+a=input("Enter Any Value")
+print(type(a))
+print(a)
+
+a=int(input("Enter Any Value"))
+print(type(a))
+print(a)
+
+a=float(input("Enter Any Value"))
+print(type(a))
+print(a)
+
+a=eval(input("Enter Any Value"))
+print(type(a))
+print(a)
+
+a=eval(input("Enter Any Value"))
+print(type(a))
+print(a)
