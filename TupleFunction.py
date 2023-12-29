2f6f921f7f4d5c02b02e57b33249240833f060b9
theteritesh <theteritesh2@gmail.com>
2023-12-29 13:23:19 +0530

    Tuple Basics and Function

---
+++ TupleFunction.py
@@ -0,0 +1,16 @@
+t=(10,20,30,40,50,60,70,80,90,10)
+
+#min
+m=min(t)
+print(m)
+
+# max
+m=max(t)
+print(m)
+# count
+print(t.count(10))
+# index
+print(t.index(50))
+# sum
+
+print(sum(t))
---
+++ TupleIteration.py
@@ -0,0 +1,9 @@
+t=(10,20,30,40,50,60,70,80)
+l=len(t)
+for i in range(l):
+    print(t[i],end="  ")
+
+print()
+
+for i in t:
+    print(i,end="  ")
